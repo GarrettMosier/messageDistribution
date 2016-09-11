5042f07540d59091dfdb2c534392bff420cc432c
Garrett Mosier <garrettmosier@gmail.com>
2016-09-10 21:00:59 -0700

    Added imports recommended by tutorial

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
+
 module Lib
     ( someFunc
     ) where
+
+import Control.Distributed.Process
+import Control.Distributed.Process
+
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
