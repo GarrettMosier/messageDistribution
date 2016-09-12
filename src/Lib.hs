5ec41e931ccb1411fe66c32a8b199e60565805a6
Garrett Mosier <garrettmosier@gmail.com>
2016-09-12 07:00:01 -0700

    Added weighted average functionality

--- src/Lib.hs
+++ src/Lib.hs
@@ -51,3 +51,11 @@ bigFunc = do
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+suma :: (Num a, Enum a) => [a] -> a 
+suma = weightedAverage [1..] 
+
+
+weightedAverage :: (Num a ) => [a] -> [a] -> a
+weightedAverage weights values = foldr (+) 0 $ zipWith (*) weights values 
