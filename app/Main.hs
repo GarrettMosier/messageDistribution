9300f6eee9753e11084a97a8c89c5ae35d8526d8
Garrett Mosier <garrettmosier@gmail.com>
2016-09-14 15:05:24 -0700

    Taking in command line arguments as Strings

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import Options.Applicative
+
+
+data CommandLineRequest = CommandLineRequest { durationToSendMessages :: String, gracePeriod :: String, seed :: String}
+
+
+commandLineParser :: Parser CommandLineRequest
+commandLineParser = CommandLineRequest <$>
+    strOption (long "send-for" <> metavar "SENDING_DURATION" <> help "How long to send messages in first part of program execution") <*>
+    strOption (long "wait-for" <> metavar "GRACE_PERIOD" <> help "How long to wait after the batch of messages was sent until calculation time.") <*> 
+    strOption (long "with-seed" <> metavar "SEED" <> help "The seed to be used in the random message generation") 
+    
+
 
 main :: IO ()
 main = bigFunc 1 1 1
